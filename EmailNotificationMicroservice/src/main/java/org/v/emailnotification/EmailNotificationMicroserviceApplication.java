a498b92928327ebf936e26accb7ebaeb447a20e5
Vighnesh Tathare <v42069@yahoo.com>
2025-10-05 20:35:40 +0530

    consumer example

---
+++ EmailNotificationMicroservice/src/main/java/org/v/emailnotification/EmailNotificationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.v.emailnotification;
+
+/**
+ * Hello world!
+ *
+ */
+public class EmailNotificationMicroserviceApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

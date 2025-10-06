c0268fcaf4ea9dc140dcaa3900f7f897de42c117
Vighnesh Tathare <v42069@yahoo.com>
2025-10-06 18:46:06 +0530

    added core for both ms

---
+++ EmailNotificationMicroservice/src/main/java/com/v/emailnotification/EmailNotificationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.v.emailnotification;
+
+/**
+ * Hello world!
+ *
+ */
+public class EmailNotificationMicroserviceApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

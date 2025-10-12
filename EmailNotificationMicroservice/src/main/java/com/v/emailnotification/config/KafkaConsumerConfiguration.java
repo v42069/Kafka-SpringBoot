d7f4c542951692fda638e079dc3de52828dbaac7
Vighnesh Tathare <v42069@yahoo.com>
2025-10-13 00:31:08 +0530

    dead letter topic

--- EmailNotificationMicroservice/src/main/java/com/v/emailnotification/config/KafkaConsumerConfiguration.java
+++ EmailNotificationMicroservice/src/main/java/com/v/emailnotification/config/KafkaConsumerConfiguration.java
@@ -1,17 +1,24 @@
 package com.v.emailnotification.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.*;
+
 import java.util.Map;
 import java.util.HashMap;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaConsumerConfiguration {
@@ -25,21 +32,50 @@ public class KafkaConsumerConfiguration {
 		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
 				environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
 		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+		//changing json to error handling
+		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
 		config.put(JsonDeserializer.TRUSTED_PACKAGES,
 				environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
 		config.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"));
+
+		//adding this for error handling
+		config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
 
 		return new DefaultKafkaConsumerFactory<>(config);
 	}
 
 	@Bean
 	ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
-			ConsumerFactory<String, Object> consumerFactory) {
+			ConsumerFactory<String, Object> consumerFactory, KafkaTemplate kafkaTemplate) {
+
+		//provide error handling capablities for kafka consumers used to handle message  exception
+		DefaultErrorHandler defaultErrorHandler=new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate));
 
 		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumerFactory);
+		//register error handler
+		factory.setCommonErrorHandler(defaultErrorHandler);
 
 		return factory;
+	}
+
+	//creating template for dead topic
+	@Bean
+	KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+		return new KafkaTemplate<>(producerFactory);
+	}
+
+	// creating producer for adding it to kafka template
+//	This method will work as configuration and configuration key value pairs.
+	// here we configured producer
+	@Bean
+	ProducerFactory<String, Object> producerFactory() {
+		Map<String, Object> config = new HashMap<>();
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
+		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+		return new DefaultKafkaProducerFactory<>(config);
 	}
 }

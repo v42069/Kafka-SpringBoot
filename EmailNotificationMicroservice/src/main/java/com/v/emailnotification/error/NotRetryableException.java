cf4318b6aea73380da3dacfe2a23732f3d02878c
Vighnesh Tathare <v42069@yahoo.com>
2025-10-16 21:41:31 +0530

    consumer retries

---
+++ EmailNotificationMicroservice/src/main/java/com/v/emailnotification/error/NotRetryableException.java
@@ -0,0 +1,17 @@
+package com.v.emailnotification.error;
+
+public class NotRetryableException extends RuntimeException {
+
+	public NotRetryableException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotRetryableException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}

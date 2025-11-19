5ff14c9c21988fd334a6c88dc60b8104a5037e6e
Vighnesh Tathare <v42069@yahoo.com>
2025-11-19 23:48:38 +0530

    kafka transactions

---
+++ deposit-microservice/src/main/java/com/v/deposit_microservice/DepositMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.v.deposit_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositMicroserviceApplication.class, args);
+	}
+
+}

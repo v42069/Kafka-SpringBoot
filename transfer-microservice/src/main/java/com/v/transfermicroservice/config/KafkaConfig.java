e7d85d3225508c11d5a40f4242bc5807ddf72219
Vighnesh Tathare <v42069@yahoo.com>
2025-11-27 23:59:56 +0530

    kafka transactions

--- transfer-microservice/src/main/java/com/v/transfermicroservice/config/KafkaConfig.java
+++ transfer-microservice/src/main/java/com/v/transfermicroservice/config/KafkaConfig.java
@@ -17,78 +17,78 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class KafkaConfig {
 
-	@Value("withdraw-money-topic")
-	private String withdrawTopicName;
+    @Value("withdraw-money-topic")
+    private String withdrawTopicName;
 
-	@Value("deposit-money-topic")
-	private String depositTopicName;
+    @Value("deposit-money-topic")
+    private String depositTopicName;
 
-	@Value("${spring.kafka.producer.bootstrap-servers}")
-	private String bootstrapServers;
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
 
-	@Value("${spring.kafka.producer.key-serializer}")
-	private String keySerializer;
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
 
-	@Value("${spring.kafka.producer.value-serializer}")
-	private String valueSerializer;
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
 
-	@Value("${spring.kafka.producer.acks}")
-	private String acks;
+    @Value("${spring.kafka.producer.acks}")
+    private String acks;
 
-	@Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
-	private String deliveryTimeout;
+    @Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
+    private String deliveryTimeout;
 
-	@Value("${spring.kafka.producer.properties.linger.ms}")
-	private String linger;
+    @Value("${spring.kafka.producer.properties.linger.ms}")
+    private String linger;
 
-	@Value("${spring.kafka.producer.properties.request.timeout.ms}")
-	private String requestTimeout;
+    @Value("${spring.kafka.producer.properties.request.timeout.ms}")
+    private String requestTimeout;
 
-	@Value("${spring.kafka.producer.properties.enable.idempotence}")
-	private boolean idempotence;
+    @Value("${spring.kafka.producer.properties.enable.idempotence}")
+    private boolean idempotence;
 
-	@Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
-	private int inflightRequests;
+    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
+    private int inflightRequests;
 
-	public Map<String, Object> producerConfigs() {
-		Map<String, Object> props = new HashMap<>();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
-		props.put(ProducerConfig.ACKS_CONFIG, acks);
-		props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
-		props.put(ProducerConfig.LINGER_MS_CONFIG, linger);
-		props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        props.put(ProducerConfig.ACKS_CONFIG, acks);
+        props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, linger);
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
 
-		props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, idempotence);
-		props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, inflightRequests);
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, idempotence);
+        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, inflightRequests);
 
-		return props;
-	}
+        return props;
+    }
 
-	@Bean
-	ProducerFactory<String, Object> producerFactory() {
-		return new DefaultKafkaProducerFactory<>(producerConfigs());
-	}
+    @Bean
+    ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
 
-	@Bean
-	KafkaTemplate<String, Object> kafkaTemplate() {
-		return new KafkaTemplate<String, Object>(producerFactory());
-	}
+    @Bean
+    KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<String, Object>(producerFactory());
+    }
 
-	@Bean
-	NewTopic createWithdrawTopic() {
-		return TopicBuilder.name(withdrawTopicName).partitions(3).replicas(3).build();
-	}
+    @Bean
+    NewTopic createWithdrawTopic() {
+        return TopicBuilder.name(withdrawTopicName).partitions(3).replicas(3).build();
+    }
 
-	@Bean
-	NewTopic createDepositTopic() {
-		return TopicBuilder.name(depositTopicName).partitions(3).replicas(3).build();
-	}
+    @Bean
+    NewTopic createDepositTopic() {
+        return TopicBuilder.name(depositTopicName).partitions(3).replicas(3).build();
+    }
 
-	@Bean
-	RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 }

e0d8fd586ffe68b7fb3634740fc00d0c3801d62a
Vighnesh Tathare <v42069@yahoo.com>
2025-12-08 01:56:54 +0530

    db transactions

--- transfer-microservice/src/main/java/com/v/transfermicroservice/config/KafkaConfig.java
+++ transfer-microservice/src/main/java/com/v/transfermicroservice/config/KafkaConfig.java
@@ -3,6 +3,7 @@ package com.v.transfermicroservice.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +14,7 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.transaction.KafkaTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -85,6 +87,11 @@ public class KafkaConfig {
     @Bean("kafkaTransactionManager")
     KafkaTransactionManager<String, Object> kafkaTransactionManager(ProducerFactory<String, Object> producerFactory) {
         return new KafkaTransactionManager<>(producerFactory);
+    }
+
+    @Bean("transactionManager")
+    JpaTransactionManager jpaTransactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
     }
 
     @Bean
